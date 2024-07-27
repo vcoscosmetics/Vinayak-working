7cb2b5c938b922deca5898e32ddf7ff151fd0b39
vcoscosmetics <vcoscosmetics@gmail.com>
2024-07-27 18:13:20 +0530

    asss

--- third-party-services.php
+++ third-party-services.php
@@ -51,102 +51,10 @@ ul{
 <body>
 
 
-    <header>
-        <div class="header-section">
 
-
-
-            <div class="contact-info">
-                <p class="email"><i class="fas fa-envelope"></i> info@vinayakayurveda.in</p>
-                <p class="phone"><i class="fas fa-phone"></i>+91 9870579944</p>
-            </div>
-            <img class="logo" src="images/logo/1.png" alt="Logo">
-
-            <div class="social-icons">
-                <a href="https://www.facebook.com" target="_blank" class="social-icon"><i
-                        class="fab fa-facebook-f"></i></a>
-                <a href="https://www.instagram.com" target="_blank" class="social-icon"><i
-                        class="fab fa-instagram"></i></a>
-                <a href="https://www.twitter.com" target="_blank" class="social-icon"><i class="fab fa-twitter"></i></a>
-            </div>
-
-        </div>
-    </header>
-
-    <div style="width: 100%; background-color: orangered; ">
-        <div style="width: 70%; margin: auto; background-color: orangered; color: whitesmoke;">
-
-            <marquee direction="left"> Discover Harmony, Embrace Health: Vinayak Ayurveda, Where Tradition Thrives!
-            </marquee>
-        </div>
-
-    </div>
-
-
-    <nav class="navbar navbar-expand-lg ">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link  " aria-current="page" href="index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="company-profile.html">Company Profile</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Product Range
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Ayurvedic classical Medicine</a></li>
-                  <li>
-                    <hr class="dropdown-divider">
-                  </li>
-    
-                  <li><a class="dropdown-item" href="#">Ayurvedic Patent Medicine</a></li>
-    
-                </ul>
-              </li>
-    
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Services
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="pcd-franchise.html">Pcd Franchise</a></li>
-                  <li>
-                    <hr class="dropdown-divider">
-                  </li>
-    
-                  <li><a class="dropdown-item" href="third-party-services.html">Third Party Manufacturing</a></li>
-    
-                </ul>
-              </li>
-    
-              <li class="nav-item">
-                <a class="nav-link" href="contact-us.html">Contact Us</a>
-              </li>
-    
-            </ul>
-    
-          </div>
-    
-          <div>
-            <a href="images/banner/1.jpeg" download="image"><button
-                style="margin-right: 1rem; cursor: pointer;  padding: 0.5rem; border-radius: 10px; border: none; color:rgb(249, 249, 249); background-color: rgb(55, 55, 55); font-weight: 500;">
-                Download Brosche
-              </button>
-            </a>
-          </div>
-        </div>
-      </nav>
-
-
-
+<?php
+include("header.php");
+?>
     <!-- End of navbar -->
 
 
